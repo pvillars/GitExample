08c41d8c94882c49ccfa4c92b1fe98628b22f78a
Petrus <pvillar@hotmail.cl>
2017-07-01 22:25:08 -0400

    rama1

--- app/src/main/java/com/gitexample/petrus/gitexample/MainActivity.java
+++ app/src/main/java/com/gitexample/petrus/gitexample/MainActivity.java
@@ -25,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();//holaa
                 //dasd
-                //jjsjskisdkdddkdk
+                //jjsjskisdkdddkdkiijlkj
+                //rama1
             }
         });
     }
